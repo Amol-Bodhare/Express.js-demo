5c178f6e3dac1c5ce5f81b99c2faddf0f1939be2
unknown <amolfeb95@gmail.com>
2018-04-04 20:43:20 -0400

    JSON Object

---
+++ quotesObject.js
@@ -0,0 +1,44 @@
+const quotes ={
+    0 : {
+      quote:"Don't cry because it's over, smile because it happened.",
+      author:"-Dr. Seuss."
+    },
+    1 : {
+      quote:"Two things are infinite: the universe and human stupidity; and I'm not sure about the universe.",
+      author:"- Albert Einstein."
+    },
+    2 : {
+      quote:"Be the change that you wish to see in the world.",
+      author:"- Mahatma Gandhi."
+    },
+    3 : {
+      quote:"Every project is an opportunity to learn, to figure out problems and challenges, to engineer and re-engineer.",
+      author:"- Amol Bodhare."
+    },
+    4 : {
+      quote:"Learning never exhausts the mind.",
+      author:"- Leonardo da Vinci."
+    },
+    5 : {
+      quote:"I have not failed. I've just found 10,000 ways that won't work.",
+      author:"- Thomas A. Edison."
+    },
+    6 : {
+      quote:"Education is the most powerful weapon which you can use to change the world.",
+      author:"- Nelson Mandela."
+    },
+    7 : {
+      quote:"Do not dwell in the past, do not dream of the future, concentrate the mind on the present moment.",
+      author:"- Buddha."
+    },
+    8 : {
+      quote:"It is during our darkest moments that we must focus to see the light.",
+      author:"- Aristotle."
+    },
+    9 : {
+      quote:"Nothing is impossible, the word itself says 'I'm possible'!",
+      author:"- Audrey Hepburn."
+    },
+  };
+
+  module.exports.quotes = quotes;
